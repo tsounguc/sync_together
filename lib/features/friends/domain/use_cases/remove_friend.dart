4a804d1a0b79031a5d57ef457d814b9303dadf94
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2025-03-14 01:38:30 -0400

    Add remove friend use case

---
+++ lib/features/friends/domain/use_cases/remove_friend.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+import 'package:sync_together/core/usecases/usecase.dart';
+import 'package:sync_together/core/utils/type_defs.dart';
+import 'package:sync_together/features/friends/domain/repositories/friend_repository.dart';
+
+class RemoveFriendRequest extends UseCaseWithParams<void, RemoveFriendRequestParams> {
+  const RemoveFriendRequest(this.repository);
+
+  final FriendRepository repository;
+
+  @override
+  ResultFuture<void> call(RemoveFriendRequestParams params) => repository.removeFriend(
+        senderId: params.senderId,
+        receivedId: params.receiverId,
+      );
+}
+
+class RemoveFriendRequestParams extends Equatable {
+  const RemoveFriendRequestParams({
+    required this.senderId,
+    required this.receiverId,
+  });
+
+  const RemoveFriendRequestParams.empty()
+      : senderId = '',
+        receiverId = '';
+
+  final String senderId;
+  final String receiverId;
+
+  @override
+  List<Object?> get props => [senderId, receiverId];
+}
