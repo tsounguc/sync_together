f33b76cfe13bd914f2dddbd46fe62f1be72030e2
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2025-03-14 10:22:02 -0400

    Add get friends use case

---
+++ lib/features/friends/domain/use_cases/get_friends.dart
@@ -0,0 +1,15 @@
+import 'package:sync_together/core/usecases/usecase.dart';
+import 'package:sync_together/core/utils/type_defs.dart';
+import 'package:sync_together/features/friends/domain/entities/friend.dart';
+import 'package:sync_together/features/friends/domain/repositories/friend_repository.dart';
+
+class GetFriends extends UseCaseWithParams<List<Friend>, String> {
+  const GetFriends(this.repository);
+
+  final FriendRepository repository;
+
+  @override
+  ResultFuture<List<Friend>> call(String params) => repository.getFriends(
+        params,
+      );
+}
