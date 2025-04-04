8e6098220da5f4e1d438dad0edd84f9fce8f3dcb
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2025-04-03 20:47:50 -0400

    Enable Picture-in-Picture Mode in AndroidManifest.xml

--- android/app/src/main/kotlin/com/example/sync_together/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sync_together/MainActivity.kt
@@ -3,3 +3,9 @@ package com.example.sync_together
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
+
+override fun onUserLeaveHint() {
+    val params = PictureInPictureParams.Builder().build()
+    enterPictureInPictureMode(params)
+}
+
