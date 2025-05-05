f5fa2bea01524399718059f1da52c81b2cdf20a8
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2025-05-05 00:28:15 -0400

    add documentation to update user profile usecase

--- lib/features/auth/domain/use_cases/update_user_profile.dart
+++ lib/features/auth/domain/use_cases/update_user_profile.dart
@@ -4,6 +4,9 @@ import 'package:sync_together/core/usecases/usecase.dart';
 import 'package:sync_together/core/utils/type_defs.dart';
 import 'package:sync_together/features/auth/domain/repositories/auth_repository.dart';
 
+/// **Use Case: UpdateUserProfile**
+///
+/// Calls the [AuthRepository] to log out the user.
 class UpdateUserProfile extends UseCaseWithParams<void, UpdateUserProfileParams> {
   UpdateUserProfile(this.repository);
   final AuthRepository repository;
@@ -18,12 +21,16 @@ class UpdateUserProfile extends UseCaseWithParams<void, UpdateUserProfileParams>
       );
 }
 
+/// **Parameters for updating user profile**
+///
+/// Includes an email and password.
 class UpdateUserProfileParams extends Equatable {
   const UpdateUserProfileParams({
     required this.action,
     required this.userData,
   });
 
+  /// Empty constructor for testing purposes.
   const UpdateUserProfileParams.empty()
       : this(
           action: UpdateUserAction.displayName,
