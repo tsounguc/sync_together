54e254d3cf224bd9c0bea3b0e045f7b59ef404f5
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2025-06-24 20:51:29 -0400

    Add appLogo in media_resources.dart

--- lib/core/resources/media_resources.dart
+++ lib/core/resources/media_resources.dart
@@ -1,23 +1,20 @@
 class MediaResources {
   const MediaResources._();
 
-  // static const _baseLogo = 'assets/logo';
   static const _baseImage = 'assets/images';
-  // static const _baseAudio = 'audio';
-  // static const _baseLottie = 'assets/lottie';
+  static const _baseIcon = 'assets/icon';
   static const _baseData = 'assets/data';
-  // static const _basePlatformsLogos = 'assets/platforms_Logos';
   static const _baseHtml = 'assets/html';
 
   // platforms
   static const platFormsData = '$_baseData/platforms.json';
-
-  // platforms logos
-  // static const platformsLogos = '$_basePlatformsLogos/logo/'
 
   // Images
   static const googleSignIn = '$_baseImage/google_sign_in.png';
 
   // HTML
   static const youtubePlayer = '$_baseHtml/youtube_player.html';
+
+  // Icons
+  static const appLogo = '$_baseIcon/app_icon.png';
 }
