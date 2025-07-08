1321f0b90675e41e9ba722f3f01b8862235d1404
Veronique Tsoungui <veroniquetsoungui@MacBookPro.lan>
2025-07-08 16:46:40 -0400

    minor changes

--- lib/features/watch_party/presentation/views/platform_video_picker_screen.dart
+++ lib/features/watch_party/presentation/views/platform_video_picker_screen.dart
@@ -149,7 +149,6 @@ class _PlatformVideoPickerScreenState extends State<PlatformVideoPickerScreen> {
     final navigator = Navigator.of(context);
     await Future<void>.delayed(const Duration(seconds: 2));
     navigator.popUntil(ModalRoute.withName('/'));
-    await Future<void>.delayed(const Duration(seconds: 2));
     await navigator.pushNamed(
       WatchPartyScreen.id,
       arguments: WatchPartyScreenArguments(party, party.platform),
